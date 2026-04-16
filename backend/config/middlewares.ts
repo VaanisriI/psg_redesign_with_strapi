f1f0928f5ee652ad11f9d1a595690e2a681e4680
VaanisriI <vaanisrideveloper@gmail.com>
2026-04-16 17:07:30 +0530

    Updated programs ui

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -3,8 +3,36 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:', 'http://127.0.0.1:1337'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:', 'http://127.0.0.1:1337'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:4321',
+        'http://localhost:4322',
+        'http://localhost:4323',
+        'http://127.0.0.1:4321',
+        'http://127.0.0.1:4322',
+        'http://127.0.0.1:1337',
+        'http://localhost:1337',
+      ],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
@@ -14,3 +42,4 @@ const config: Core.Config.Middlewares = [
 ];
 
 export default config;
+
